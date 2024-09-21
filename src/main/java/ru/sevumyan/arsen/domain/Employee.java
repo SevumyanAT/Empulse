cff46d65cd23a9de24a9260ce432a0758f40e407
Arsen  Sevumyan <alarseniy@gmail.com>
2024-09-21 21:45:50 +0300

    EMPULSE-8 Эдпоинт получения наставников

--- src/main/java/ru/sevumyan/arsen/domain/Employee.java
+++ src/main/java/ru/sevumyan/arsen/domain/Employee.java
@@ -34,7 +34,7 @@ public class Employee {
     private String bankAccount;
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mentor_id")
-    private Employee mentorId;
+    private Employee mentor;
     @ManyToOne
     @JoinColumn(name = "positions_id")
     private Position position;
