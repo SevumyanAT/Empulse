9603006c4909e2f48ef55961263274a41ed76fc8
Arsen  Sevumyan <alarseniy@gmail.com>
2024-10-29 17:33:49 +0200

    EMPULSE-10    Добавление библиотеки MapStruct и использование MapStruct для мапинга DTO в объекты и наоборот

--- src/main/java/ru/sevumyan/arsen/EmpulseApplication.java
+++ src/main/java/ru/sevumyan/arsen/EmpulseApplication.java
@@ -1,10 +1,7 @@
 package ru.sevumyan.arsen;
 
-import lombok.extern.log4j.Log4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class EmpulseApplication {
