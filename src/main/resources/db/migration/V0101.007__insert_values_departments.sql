6cfb706dcc928e688ed936313d22b3c8127e9816
Arsen  Sevumyan <alarseniy@gmail.com>
2024-12-12 20:29:00 +0200

    EMPULSE-11    Отказ от Legacy-функциональности

--- src/main/resources/db/migration/V0101.007__insert_values_departments.sql
+++ src/main/resources/db/migration/V0101.007__insert_values_departments.sql
@@ -1,10 +1,10 @@
-    insert into department(department_location) values ('Grozniy');
-    insert into department(department_location) values ('Moscow');
-    insert into department(department_location) values ('St.Petersburg');
-    insert into department(department_location) values ('Ekaterinburg');
-    insert into department(department_location) values ('Balashikha');
-    insert into department(department_location) values ('Pavlovsky-Posad');
-    insert into department(department_location) values ('London');
-    insert into department(department_location) values ('Dubai');
+insert into department(department_location) values ('Grozniy');
+insert into department(department_location) values ('Moscow');
+insert into department(department_location) values ('St.Petersburg');
+insert into department(department_location) values ('Ekaterinburg');
+insert into department(department_location) values ('Balashikha');
+insert into department(department_location) values ('Pavlovsky-Posad');
+insert into department(department_location) values ('London');
+insert into department(department_location) values ('Dubai');
 
 
